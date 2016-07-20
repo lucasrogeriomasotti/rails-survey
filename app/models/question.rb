1523032bad6da6007cf0dff05d9eaf1424e6b363
Lucas Masotti <lucasmasotti@gmail.com>
2016-07-20 15:46:50 -0300

    Adding question types with single table inheritance

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
+  default_scope { order(:position) }
+  validates :survey, :question_text, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :questions
   resources :surveys
   root to: 'visitors#index'
+  get 'users/sign_up' => redirect('/404.html')
   devise_for :users
   resources :users
 end
